d9dd70771dc1909f2a5a7c71d653e72a97c83618
Callum Mackay <mackaycallum99@gmail.com>
2020-12-12 16:26:04 +0000

    Ran gdformat on wall.gd and test_wall.gd after previous commit

--- logic/wall.gd
+++ logic/wall.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 signal reset_ball
 
+
 func _on_wall_area_entered(area):
 	if area.name == "Ball":
 		#oops, ball went out of game place, reset
--- test/unit/test_wall.gd
+++ test/unit/test_wall.gd
@@ -3,12 +3,15 @@ extends "res://addons/gut/test.gd"
 var wall_class = load("res://logic/wall.gd")
 var ball_class = load("res://logic/ball.gd")
 
+
 func test_ball_should_reset_on_hitting_wall():
 	var wall = add_child_autofree(wall_class.new())
 	watch_signals(wall)
 	var mock_area = autofree(Area2D.new())
 	wall._on_wall_area_entered(mock_area)
-	assert_signal_not_emitted(wall, "reset_ball", "Ball should not be reset on generic area hitting wall")
+	assert_signal_not_emitted(
+		wall, "reset_ball", "Ball should not be reset on generic area hitting wall"
+	)
 	# crashes if ball isn't added to scene
 	# the ball should always be in the scene, so this is fine
 	var mock_ball = add_child_autofree(ball_class.new())
@@ -16,4 +19,3 @@ func test_ball_should_reset_on_hitting_wall():
 	mock_ball.name = "Ball"
 	wall._on_wall_area_entered(mock_ball)
 	assert_signal_emitted(wall, "reset_ball", "Ball should be reset on hitting wall")
-
