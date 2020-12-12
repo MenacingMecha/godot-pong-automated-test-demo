6bcdbe4823eafcbea76a5c66a7fc3ef3fba96256
Callum Mackay <mackaycallum99@gmail.com>
2020-12-12 16:23:53 +0000

    Added test for the ball resetting on hitting a wall

--- logic/wall.gd
+++ logic/wall.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
+signal reset_ball
+
 func _on_wall_area_entered(area):
 	if area.name == "Ball":
 		#oops, ball went out of game place, reset
+		emit_signal("reset_ball")
 		area.reset()
