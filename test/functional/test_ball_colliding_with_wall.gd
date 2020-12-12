efa8417039cb570d7d56993397b8cfd19ab88438
Callum Mackay <mackaycallum99@gmail.com>
2020-12-12 21:24:04 +0000

    Added functional test for ball resetting on hitting a back wall

---
+++ test/functional/test_ball_colliding_with_wall.gd
@@ -0,0 +1,21 @@
+extends "res://addons/gut/test.gd"
+
+const START_POSITION_LEFT := Vector2(20, 50)
+const START_POSITION_RIGHT := Vector2(40, 50)
+const MAX_YIELD_WAIT_TIME := 1
+
+var ball_packedscene = load("res://ball.tscn")
+var wall_packedscene = load("res://wall.tscn")
+
+func test_ball_resets_on_hitting_wall():
+	var wall = autofree(wall_packedscene.instance())
+	wall.position = START_POSITION_LEFT
+	add_child(wall)
+	var ball = autofree(ball_packedscene.instance())
+	ball.position = START_POSITION_RIGHT
+	add_child(ball)
+	assert_true(ball.direction.x < 0, "ball should be moving left")
+	watch_signals(wall)
+	yield(yield_to(wall, "reset_ball", MAX_YIELD_WAIT_TIME), YIELD)
+	assert_signal_emitted(wall, "reset_ball")
+	assert_eq(ball.position, ball._initial_pos, "ball position should be reset")
