94f7f6367190aaf933a529895eaa4c7d6e91c3ba
Callum Mackay <mackaycallum99@gmail.com>
2020-12-12 22:26:48 +0000

    Test runner scene is now switched to from a scene toggle singleton rather than always overlayed on top of the game scene

---
+++ logic/scene_toggle.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var pong_scene_path = "res://pong.tscn"
+var test_runner_scene_path = "res://test/gut.tscn"
+
+
+func _input(_event):
+	if Input.is_action_just_pressed("kb_1"):
+		get_tree().change_scene(pong_scene_path)
+	if Input.is_action_just_pressed("kb_2"):
+		get_tree().change_scene(test_runner_scene_path)
